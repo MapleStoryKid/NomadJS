e2562bade841cca9353e03448eecf07bcb8bb4b2
MapleStoryKid <101703192+MapleStoryKid@users.noreply.github.com>
2023-03-05 13:31:57 +0900

    3/5

--- js/background.js
+++ js/background.js
@@ -2,8 +2,8 @@ const images = ["1.jpg", "2.jpg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+const main = document.querySelector("main");
 
-bgImage.src = `img/${chosenImage}`;
+main.style.background = `url(../img/${chosenImage})`;
 
-document.body.appendChild(bgImage);
+
